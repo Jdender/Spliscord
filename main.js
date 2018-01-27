0673e23f52259c6b5429d9e27684c7376d16aefb
jdender <jdenderplays@gmail.com>
2018-01-27 16:44:16 -0500

    Update verson check in main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-if (process.version.slice(1).split('.')[0] < 8) throw new Error('Node 8.0.0 or higher is required. Update Node.');
+const version = process.version.slice(1).split('.');
+if (version[0] < 8 || version[1] < 9) throw new Error('Node 8.9.0 or higher is required. Update Node.');
 
 //#region Set up client and requires.
 const client = new(require('discord.js')).Client();
