43e1fe8cf73e27af994467940ac165a18e651a04
jdender <jdenderplays@gmail.com>
2018-01-28 13:14:43 -0500

    Make a amazing one liner

--- requires.js
+++ requires.js
@@ -7,14 +7,24 @@ const requires = {
     nodeutil: require('util'),
     Enmap: require('enmap'),
     EnmapLevel: require('enmap-level'),
+    path: require('path'),
 }
 //#endregion
 
 //#region Patch or construct modules
 requires.fs.readdir = requires.nodeutil.promisify(requires.fs.readdir);
+requires.fs.stat = requires.nodeutil.promisify(requires.fs.stat);
 
 const provider = new requires.EnmapLevel({ name: 'guildIndex' })
 requires.guildIndex = new requires.Enmap({ provider });
+//#endregion
+
+//#region Util Functions
+const walk = async d => (await requires.fs.stat(d)).isDirectory() ? await Promise.all((await requires.fs.readdir(d)).map(async f => await walk(requires.path.join(d, f)))) : d;
+
+Object.assign(requires, {
+    walk
+});
 //#endregion
 
 // Any modules not needed
