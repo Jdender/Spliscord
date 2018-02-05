980082e5426d8cd174a97c472947a42b1f851e2b
jdender <jdenderplays@gmail.com>
2018-02-04 20:58:55 -0500

    Add more events to main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,29 @@ import { Config } from './interfaces/config';
 //#endregion
 
 //#region Unpack/Init
-const { on, registerEvents } = Events;
+const { on, once, registerEvents } = Events;
 //#endregion
 
 //#region Because typescript
 class Spliscord extends Client {
+
+    public prefixMention: RegExp;
+    public inviteLink: string;
+
     public constructor(public config: Config) {
         super();
-
         registerEvents(this);
+    }
+
+    @once('ready')
+    private _onceReady(): void {
+        this.prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+        this.inviteLink = `https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot`;
+    }
+
+    @on('ready')
+    private _onReady(): void {
+        console.info(`[info] Runing in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
     }
 
     @on('debug')
@@ -21,6 +35,15 @@ class Spliscord extends Client {
         console.info(message);
     }
 
+    @on('warn')
+    private _onWarn(warning: string): void {
+        console.warn(warning);
+    }
+
+    @on('error')
+    private _onError(error: string): void {
+        console.error(error);
+    }
 }
 //#endregion
 
