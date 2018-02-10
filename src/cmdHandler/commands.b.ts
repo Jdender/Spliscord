3ada518e50ecbd1423587ba672b3a7c009e09587
jdender <jdenderplays@gmail.com>
2018-02-10 17:13:52 -0500

    Add command barrel

---
+++ src/cmdHandler/commands.b.ts
@@ -0,0 +1,4 @@
+export { Command, CommandMessage } from '../cmdHandler/commands.i';
+export { Spliscord as Client } from '../client/main';
+export { Message } from 'discord.js';
+export { promisified as phin } from 'phin';
