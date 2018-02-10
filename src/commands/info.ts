66122759117e172ff4453798281d6ba956237696
jdender <jdenderplays@gmail.com>
2018-02-10 17:24:42 -0500

    Add info command

---
+++ src/commands/info.ts
@@ -0,0 +1,13 @@
+import { Command, CommandMessage, Client, Message } from '../cmdHandler/commands.b';
+
+const info: Command = {
+    name: 'info',
+    aliases: ['information', 'about', ''],
+    cooldown: 3,
+    description: 'Ping!',
+    execute(client: Client, message: CommandMessage) {
+        message.channel.send('For information or source for the bot please see the readme on github: https://github.com/jdenderplays/Spliscord/#readme');
+    },
+}
+
+export default info;
