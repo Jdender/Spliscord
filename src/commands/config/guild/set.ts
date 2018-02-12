c428df083dd534c2cd6d1406375b5f4e355fde8f
jdender <jdenderplays@gmail.com>
2018-02-11 20:36:12 -0500

    Add `config.user.set` and `config.guild.set` commands

---
+++ src/commands/config/guild/set.ts
@@ -0,0 +1,24 @@
+import { Command, CommandMessage, Client, Message, StorageTypeKeys } from '../../../cmdHandler/commands.b';
+
+const set: Command = {
+    name: 'config.guild.set',
+    aliases: ['conf.guild.set', 'conf.g.set'],
+    cooldown: 5,
+    description: 'Use flags to set guild config options.',
+    args: false,
+    userConf: false,
+    guildConf: true,
+    guildOnly: true,
+    execute(client: Client, message: CommandMessage) {
+        
+        if (typeof message.args.prefix === 'string') {
+            client.db.dispatch({
+                type: StorageTypeKeys.SET_GUILD_PREFIX,
+                id: message.guild.id,
+                prefix: message.args.prefix,
+            });
+        }
+    },
+}
+
+export default set;
---
+++ src/commands/config/user/set.ts
@@ -0,0 +1,24 @@
+import { Command, CommandMessage, Client, Message, StorageTypeKeys } from '../../../cmdHandler/commands.b';
+
+const set: Command = {
+    name: 'config.user.set',
+    aliases: ['conf.user.set', 'conf.u.set'],
+    cooldown: 5,
+    description: 'Use flags to set user config options.',
+    args: false,
+    userConf: true,
+    guildConf: false,
+    guildOnly: false,
+    execute(client: Client, message: CommandMessage) {
+        
+        if (typeof message.args.prefix === 'string') {
+            client.db.dispatch({
+                type: StorageTypeKeys.SET_USER_PREFIX,
+                id: message.author.id,
+                prefix: message.args.prefix,
+            });
+        }
+    },
+}
+
+export default set;
