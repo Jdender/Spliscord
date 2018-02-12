b7df84b73c676021a0148822958219d066ab52c6
jdender <jdenderplays@gmail.com>
2018-02-11 20:34:51 -0500

    Add storage action keys import/export to command barell

--- src/cmdHandler/commands.b.ts
+++ src/cmdHandler/commands.b.ts
@@ -2,3 +2,4 @@ export { Command, CommandMessage } from '../cmdHandler/commands.i';
 export { Spliscord as Client } from '../client/main';
 export { Message } from 'discord.js';
 export { promisified as phin } from 'phin';
+export { TypeKeys as StorageTypeKeys} from '../storage/actions';
