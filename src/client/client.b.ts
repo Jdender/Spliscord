ce0b381e018cfe6e7a72e37b5e544595e9fabe8c
jdender <jdenderplays@gmail.com>
2018-02-16 19:02:39 -0500

    Update client for new logger

--- src/client/client.b.ts
+++ src/client/client.b.ts
@@ -1,9 +1,9 @@
-export { Collection } from 'discord.js';
+export { Collection, Client } from 'discord.js';
 export { BotConfig } from './config.i';
 export { walk } from '../util/oneline';
 export { readdirAsync } from '../util/fsAsync';
 export { Command } from '../cmdUtil/commands.i';
 export { flattenDeep } from 'lodash';
-export { Logger } from '../logger/main';
+export { logger } from '../logger/main';
 export { execute as executeCmd } from '../handler/main';
 export { storage } from '../storage/main';
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,52 +1,58 @@
 //#region Because barrels
 import {
     Collection,
+    Client,
     BotConfig,
     walk,
     readdirAsync,
     Command,
     flattenDeep,
-    Logger,
+    logger,
     executeCmd,
     storage,
 } from './client.b';
 //#endregion
 
 //#region Because typescript
-export class Spliscord extends Logger {
+export class Spliscord extends Client {
 
     public commands: Collection < string, Command > = new Collection();
     public cooldowns: Collection < string, Collection < string, any > > = new Collection();
 
     public storage = storage;
 
+    public prefixMention: RegExp;
+    public inviteLink: string;
+
     public constructor(public config: BotConfig) {
         super();
 
-        this._registerEvents();
-        this._import();
+        logger(this);
+
+        this.import();
 
         this.on('message', (message: any) => executeCmd(this, message));
 
         this.login(require(config.token.path)[config.token.name]);
     }
 
-    private async _import(): Promise < void > {
+    public async import(): Promise < void > {
 
         //#region Command Importer
-        const rawCommandFiles: string[] = flattenDeep(await walk('./dist/commands/'));
+        const rawCommandFiles: string[] = flattenDeep(await walk('./src/commands/'));
         const commandFiles: string[] = rawCommandFiles.filter((file: string) => file.split('.')[2] !== 'map');
 
         console.info(`[init] [load] Loading ${commandFiles.length} commands.`);
 
         for (const file of commandFiles) {
-            if (file.split('.')[1] !== 'js') return;
+            if (file.split('.')[1] !== 'ts') continue;
 
             const { default: command } = require(`../../${file}`); // The `..` is needed.
 
             this.commands.set(command.name, command);
         }
         //#endregion
+
     }
 }
 //#endregion
