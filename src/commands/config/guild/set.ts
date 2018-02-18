ef133e85d63027e5f2f1ff61be178a301969ff00
jdender <jdenderplays@gmail.com>
2018-02-18 11:58:09 -0500

    Update confing.guild.set for new actions

--- src/commands/config/guild/set.ts
+++ src/commands/config/guild/set.ts
@@ -11,14 +11,60 @@ const set: Command = {
     guildConf: true,
     guildOnly: true,
     execute(client: Client, message: CommandMessage) {
-        
+        let newSet = false;
+        const reply: string[] = [];
+
         if (typeof message.args.prefix === 'string') {
+            newSet = true;
+            
             client.storage.dispatch({
                 type: StorageTypeKeys.SET_GUILD_PREFIX,
                 id: message.guild.id,
                 prefix: message.args.prefix,
             });
+            reply.push(`\`--prefix\`: Set this guild's prefix to "${message.args.prefix}"`)
         }
+
+        if (typeof message.args.admin === 'string') {
+            newSet = true;
+
+            const role = message.guild.roles.get(message.args.admin);
+            if (!role) {
+                reply.push(`\`--admin\`: Unable to find role.`);
+            } else {
+                client.storage.dispatch({
+                    type: StorageTypeKeys.SET_GUILD_ADMIN_ROLE,
+                    id: message.guild.id,
+                    role: message.args.admin,
+                });
+                reply.push(`\`--admin\`: Set this guild's admin role to "${role.name}"`);
+            }
+        }
+
+        if (typeof message.args.mod === 'string') {
+            newSet = true;
+
+            const role = message.guild.roles.get(message.args.mod);
+            if (!role) {
+                reply.push(`\`--mod\`: Unable to find role.`);
+            } else {
+
+                client.storage.dispatch({
+                    type: StorageTypeKeys.SET_GUILD_MOD_ROLE,
+                    id: message.guild.id,
+                    role: message.args.mod,
+                });
+                reply.push(`\`--mod\`: Set this guild's mod role to "${role.name}"`);
+            }
+        }
+
+        if (newSet) {
+            reply.push('Edited config saved to storage.');
+            message.channel.send(reply);
+        } else {
+            message.channel.send('No flags found.');
+        }
+
     },
 }
 
