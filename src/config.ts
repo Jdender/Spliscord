79d6f8e278f4dd41fc7163dd2b141d9370ceef68
jdender <jdenderplays@gmail.com>
2018-02-17 21:29:33 -0500

    Add contributors to config

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,15 @@ export default {
     client: {
         disableEveryone: true,
     },
+    contributors: [{
+            name: 'jdenderplays',
+            id: '250432205145243649',
+            roles: [4, 5, 6]
+        },
+        {
+            name: 'House Master',
+            id: '271434593939226624',
+            roles: [4, 5]
+        }
+    ]
 }
