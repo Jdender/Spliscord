8823ea0737ad7c51c77220a388414391d06547aa
jdender <jdenderplays@gmail.com>
2018-02-19 11:44:34 -0500

    Simplify logger

--- src/logger/main.ts
+++ src/logger/main.ts
@@ -7,25 +7,17 @@ interface LoggerClient extends Client {
 
 export function logger(client: LoggerClient) {
 
-    client.once('ready', () => {
+    client.on('ready', () => {
+        console.info(`[info] Runing in ${client.channels.size} channels on ${client.guilds.size} guilds, for a total of ${client.users.size} users.`);
+
         client.prefixMention = new RegExp(`^<@!?${client.user.id}> `);
         client.inviteLink = `https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot`;
     });
 
-    client.on('ready', () => {
-        console.info(`[info] Runing in ${client.channels.size} channels on ${client.guilds.size} guilds, for a total of ${client.users.size} users.`);
-    });
+    client.on('debug', console.info);
 
-    client.on('debug', (message: string) => {
-        console.info(message);
-    });
+    client.on('warn', console.warn);
 
-    client.on('warn', (warning: string) => {
-        console.warn(warning);
-    });
-
-    client.on('error', (error: string) => {
-        console.error(error);
-    });
+    client.on('error', console.error);
 
 }
