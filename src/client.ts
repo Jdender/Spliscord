98766bbabd717db71d580ab299160be585553caa
jdender <jdenderplays@gmail.com>
2018-02-24 16:57:43 -0500

    More decorators

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,24 @@
-import { Client } from 'discord.js';
+import { Client, Collection } from 'discord.js';
 import { BotConfig } from './configs';
 import { logger } from './logger';
+import { loader } from './loader';
+import { handler, Command } from './handler';
 
-@logger
+@loader
+@handler
+@logger // Logger needs to be added first.
+
 export class Spliscord extends Client {
 
     prefixMention: RegExp;
     inviteLink: string;
+
+    commands = new Collection < string,
+    Command > ();
+
+    cooldowns = new Collection < string,
+    Collection < string,
+    number > > ();
 
     constructor(public config: BotConfig) {
         super(config.client);
