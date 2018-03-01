f314eeac3efc39b5b696bec5dd59804d9c5a142a
jdender <jdenderplays@gmail.com>
2018-02-28 20:11:31 -0500

    Add walkflat function

--- src/oneline.ts
+++ src/oneline.ts
@@ -2,12 +2,15 @@
 import { join } from 'path';
 import { readdir, stat } from 'fs';
 import { promisify } from 'util';
+import flattenDeep = require('lodash/flattenDeep');
 
 export const readdirAsync = promisify(readdir);
 export const statAsync = promisify(stat);
 
 export type Constructor < T > = new(...args: any[]) => T;
 
-export const walk = async (dir: string): Promise<any> => (await statAsync(dir)).isDirectory() ? await Promise.all((await readdirAsync(dir)).map(async file => await walk(join(dir, file)))) : dir;
+export const walk = async (dir: string): Promise < any > => (await statAsync(dir)).isDirectory() ? await Promise.all((await readdirAsync(dir)).map(async file => await walk(join(dir, file)))) : dir;
+
+export const walkflat = async (dir: string): Promise < string[] > => flattenDeep(await walk(dir));
 
 export const pipe = (...funcs: Function[]) => funcs.reduce((f1, f2) => (...args: any[]) => f2(f1(...args)));
