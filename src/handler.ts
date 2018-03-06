7979ef7c77d6a0ffdc16d732070848de56a91aad
jdender <jdenderplays@gmail.com>
2018-03-05 19:41:25 -0500

    Add executer to handleOnMessage() and add MessageCommandMeta constructor and findCommand()

--- src/handler.ts
+++ src/handler.ts
@@ -3,13 +3,14 @@ import { Spliscord } from './client';
 import { UserConfig, GuildConfig } from './configs';
 import { Constructor } from './oneline';
 import { Message } from 'discord.js';
+import isEqual = require('lodash/isEqual');
 const { on, once, registerEvents } = Events;
 
 
 export interface Command {
     name: string;
     description: string;
-    aliases: string[] | null;
+    aliases: string[];
     usage: string;
 
     cooldown: number;
@@ -29,8 +30,30 @@ export interface Command {
 
 export class MessageCommandMeta {
 
-    constructor() {
-        //TODO
+    constructor(client: Spliscord, message: Message) {
+
+        this.prefix = ',';
+
+        this.command = this.findCommand(client, message);
+    }
+
+    private findCommand(client: Spliscord, message: Message): string {
+
+        const split = message.content.slice(this.prefix.length).split(/\s+/g);
+        const path = [];
+
+        let command = '';
+
+        for (let i = 0; i < client.config.maxSubCommandDepth && i < split.length; i++) {
+            path.push(split[i]);
+
+            for (const name of client.commandNameCache) {
+                if (isEqual(path, name))
+                    command = path.join('.');
+            }
+        }
+
+        return command;
     }
 
     command: string;
@@ -51,13 +74,36 @@ export function handler < T extends Constructor < Spliscord > > (Main: T) {
         }
 
         @on('message')
-        handleOnMessage(message: Message): boolean {
+        async handleOnMessage(message: Message): Promise < boolean > {
 
             if (message.author.id === '1') return (console.warn(message.content), false); // Warn then Ignore Clyde
             if (message.author.bot) return false; // Ignore Bots
-            if (message.guild.me.permissionsIn(message.channel).missing(['SEND_MESSAGES'])) return false; // Ignore if can't respond
 
-            return true;//TODO
+            const meta = new MessageCommandMeta(this, message);
+
+            const command = this.commands.get(meta.command) || // The !! is casting to bool to toss the null type
+                this.commands.find(cmd => !!cmd.aliases && cmd.aliases.includes(meta.command));
+
+            if (!command) return false;
+
+
+            try {
+
+                if (command.execute.constructor.name === 'AsyncFunction')
+                    await command.execute(this, message, meta)
+                else
+                    command.execute(this, message, meta);
+
+                return true;
+
+            } catch (error) {
+
+                console.error(error);
+                message.channel.send(`There was an error trying to execute the \`${command.name}\` command.`);
+
+                return false;
+            }
+
         }
 
     }
