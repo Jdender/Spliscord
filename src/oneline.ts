48a5107e65abc96348eaafb59e342c2402d784e9
jdender <jdenderplays@gmail.com>
2018-03-10 12:23:31 -0500

    Add String.prototype.multiSearch

--- src/oneline.ts
+++ src/oneline.ts
@@ -4,13 +4,45 @@ import { readdir, stat } from 'fs';
 import { promisify } from 'util';
 import flattenDeep = require('lodash/flattenDeep');
 
+
 export const readdirAsync = promisify(readdir);
 export const statAsync = promisify(stat);
 
+
 export type Constructor < T > = new(...args: any[]) => T;
 
-export const walk = async (dir: string): Promise < any > => (await statAsync(dir)).isDirectory() ? await Promise.all((await readdirAsync(dir)).map(async file => await walk(join(dir, file)))) : dir;
 
-export const walkflat = async (dir: string): Promise < string[] > => flattenDeep(await walk(dir));
+export const walk = async (dir: string): Promise < any > =>
+    (await statAsync(dir)).isDirectory() ?
+    await Promise.all(
+        (await readdirAsync(dir))
+        .map(async file =>
+            await walk(join(dir, file)))) :
+    dir;
 
-export const pipe = (...funcs: Function[]) => funcs.reduce((f1, f2) => (...args: any[]) => f2(f1(...args)));
+
+export const walkflat = async (dir: string): Promise < string[] > =>
+    flattenDeep(await walk(dir));
+
+
+export const pipe = (...funcs: Function[]) =>
+    funcs.reduce(
+        (f1, f2) =>
+        (...args: any[]) =>
+        f2(f1(...args))
+    );
+
+
+declare global {
+    interface String {
+        multiSearch(pattern: RegExp): RegExpExecArray[]
+    }
+}
+
+String.prototype.multiSearch = function(pattern) {
+    return this
+        .match(new RegExp(pattern.source, pattern.flags)) !
+        .map(match =>
+            new RegExp(pattern.source, pattern.flags)
+            .exec(match) !);
+}
