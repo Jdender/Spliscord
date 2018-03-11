3c5728dca5c5466a6be3bc2f032aba8171facc4c
jdender <jdenderplays@gmail.com>
2018-03-11 16:21:25 -0400

    One more quick update

--- src/oneline.ts
+++ src/oneline.ts
@@ -2,8 +2,6 @@
 import { join } from 'path';
 import { readdir, stat } from 'fs';
 import { promisify } from 'util';
-import flattenDeep = require('lodash/flattenDeep');
-import { Spliscord } from './client';
 import { inspect } from 'util';
 
 
@@ -28,6 +26,7 @@ export const walk = async (dir: string): Promise < string | RecursiveArray < str
     dir;
 
 
+import flattenDeep = require('lodash/flattenDeep');
 export const walkflat = async (dir: string, _w: string | RecursiveArray < string > ): Promise < string[] > =>
     typeof(_w = await walk(dir)) === 'string' ? [] :
     flattenDeep < string > (_w);
@@ -41,13 +40,13 @@ export const pipe = (...funcs: Function[]) =>
     );
 
 
-export const clean = async (client: Spliscord, text: any, depth ? : number | null, color ? : boolean, ): Promise < string > => (
+export const clean = async ({ token }: { token: string }, text: any, depth ? : number | null, color ? : boolean, ): Promise < string > => (
     text instanceof Promise && (text = await text),
     typeof text !== 'string' && (text = inspect(text, false, depth, color)),
     text
     .replace(/`/g, '`' + String.fromCharCode(8203))
     .replace(/@/g, '@' + String.fromCharCode(8203))
-    .replace(client.token, 'mfa.VkO_2v3T--NO--lWetW_tjND--TOKEN--QFTm--FOR--zq9PH--YOU--tG')
+    .replace(token, 'mfa.VkO_2v3T--NO--lWetW_tjND--TOKEN--QFTm--FOR--zq9PH--YOU--tG')
 )
 //#endregion
 
