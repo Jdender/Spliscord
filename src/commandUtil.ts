470e642ed2c55b9e880dba2970da1d0f1679918f
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-12 18:31:49 -0400

    Not this again /s

--- src/commandUtil.ts
+++
@@ -1,5 +0,0 @@
-export { Spliscord } from './client';
-export { Command } from './handler';
-export { MessageCommandMeta } from './msgCmdMeta';
-export { Message } from 'discord.js';
-export { clean } from './oneline';
