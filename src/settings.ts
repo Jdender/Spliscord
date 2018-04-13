ae23b11a8d25d0101421e3fb056ec38926c62751
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-13 19:37:50 -0400

    Add settings.ts file for interfaces and clases

---
+++ src/settings.ts
@@ -0,0 +1,42 @@
+import { ClientOptions } from 'discord.js';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+export interface BotConfig {
+    client: ClientOptions;
+    commandPath: string;
+    maxSubCommandDepth: number;
+    contributors: Array<{
+        name: string;
+        id: string;
+        roles: number[];
+    }>;
+}
+
+@Entity()
+export class UserConfig {
+
+    @PrimaryColumn('text')
+    public id: string;
+
+    @Column({ type: 'text', nullable: true })
+    public prefix?: string;
+}
+
+@Entity()
+export class GuildConfig {
+
+    @PrimaryColumn('text')
+    public id: string;
+
+    @Column({ type: 'text', nullable: true })
+    public prefix?: string;
+
+    @Column({ type: 'text', nullable: true })
+    public adminRole?: string;
+
+    @Column({ type: 'text', nullable: true })
+    public modRole?: string;
+
+    @Column({ type: 'text', nullable: true })
+    public joinRole?: string;
+}
