81c71f556c7d0667600e2701b0b107ef500ecdde
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-13 19:38:11 -0400

    Add db connection and redo promise chain

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,31 @@ import Chalk from 'chalk';
 import { Client } from 'discord.js';
 import { readdirSync, statSync  } from 'fs';
 import klaw = require('klaw');
+import { createConnection } from 'typeorm';
+import { GuildConfig, UserConfig } from './settings';
 
 const client = new Client();
 
-client.on('ready', () => {
+// Wait for the client to be ready
+new Promise(resolve => client.once('ready', resolve))
+.then(() => Promise.all([ // Run in parralel
 
-    console.log('ready');
+    // Make connection to db
+    createConnection({
+        name: 'spliscord',
+        database: './.data/store.sqlite',
+        type: 'sqlite',
+        entities: [
+            UserConfig, GuildConfig,
+        ],
+        synchronize: true,
+    })
+    .then(db => {
+        client.userConf = db.getRepository(UserConfig);
+        client.guildConf = db.getRepository(GuildConfig);
+    }),
 
-    // We could use callbacks, but promises are nicer
+    // Walk and import files
     new Promise<string[]>((resolve, reject) => {
 
         const items: string[] = []; // Need to say string[] so ts doesn't make it never[]
@@ -18,13 +35,11 @@ client.on('ready', () => {
         .on('data', item => !item.stats.isDirectory() && items.push(item.path)) // If not dir add to item array
         .on('end', () => resolve(items)) // When done return item array
         .on('error', reject); // Just pass the raw reject function
-
     })
     .then(files => files.map(file => import(file))) // Map paths to their import promises
-    .then(files => Promise.all(files)) // Await all imports
-    .then(files => files.forEach(file => file.default(client))) // Run their default exports with the client as an arg
-    .catch(console.error); // Allways catch promises
-
-});
+    .then(files => Promise.all(files)), // Await all imports
+]))
+// Run their default exports with the client as an arg after imported and db is ready
+.then(([, files]) => files.forEach(file => file.default(client))),
 
 client.login(process.env.TOKEN);
