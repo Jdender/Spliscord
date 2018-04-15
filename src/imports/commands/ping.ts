c1d5286e27d40b59d23e794db242bc827d5ee8d1
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-15 17:43:06 -0400

    Add ping command

--- src/imports/commands/ping.ts
+++ src/imports/commands/ping.ts
@@ -1,4 +1,24 @@
-import { Client } from 'discord.js';
+import { Client, Message } from 'discord.js';
 
 export default (client: Client) => {
+
+    client.registry.addCommand({
+        name: 'ping',
+        description: 'Test the bot\'s latency.',
+        aliases: ['pong', 'latency'],
+        usage: '',
+        cooldown: 3,
+        permissions: 0,
+        args: null,
+        checks: {
+            guildOnly: false,
+        },
+    });
+
+    client.registry.on('ping', async ({message}) => {
+        const pingMessage = await message.channel.send('Pinging...') as Message;
+
+        // tslint:disable-next-line:max-line-length
+        pingMessage.edit(`Ponged. | Our message round-trip ping is **${Math.abs(pingMessage.createdTimestamp - message.createdTimestamp)} ms**. ${client.ping ? `My heartbeat ping to discord is **${Math.round(client.ping)} ms**.` : ''}`);
+    });
 };
