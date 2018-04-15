7ea459c0f0a47632d5612a502264a3711be4beef
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-15 18:44:16 -0400

    Split order handler into functions

--- src/imports/core/handler.ts
+++ src/imports/core/handler.ts
@@ -6,76 +6,89 @@ import { filter, flatMap, groupBy } from 'rxjs/operators';
 import { Order } from '../../classes/registry';
 import { GuildConfig, UserConfig } from '../../classes/settings';
 
+async function getConf(client: Client, message: Message): Promise<[UserConfig, GuildConfig | 'DM']> {
+    // Get userConf or make userConf
+    const userConf: UserConfig = await client.userConf.findOneById(message.author.id)
+    || await client.userConf.save({ id: message.author.id });
+
+    // If dm set guildConf to 'DM'
+    const guildConf: GuildConfig | 'DM' =
+    (message.channel.type !== 'text') ? 'DM' :
+
+    // Get guildConf or make guildConf
+    await client.guildConf.findOneById(message.guild.id)
+    || await client.guildConf.save({ id: message.guild.id });
+
+    return [userConf, guildConf];
+}
+
+function getPrefix(
+    client: Client, message: Message,
+    userConf: UserConfig, guildConf: GuildConfig | 'DM',
+    ): string | null {
+
+    // No prefix in dms
+    if (guildConf === 'DM')
+        return '';
+
+    // Find prefix if not dms
+
+    // Var to store the resolved prefix
+    let prefix = '';
+
+    // Array to store found prefixes
+    const prefixes: string[] = [];
+
+    // If userConf has prefix push it
+    if (userConf.prefix)
+        prefixes.push(userConf.prefix);
+
+    // If guildConf has prefix push it
+    if (guildConf.prefix)
+        prefixes.push(guildConf.prefix);
+
+    // For each of the prefixes check if the message starts with it
+    for (const thisPrefix of prefixes)
+        if (message.content.startsWith(thisPrefix))
+            prefix = thisPrefix;
+
+    // Check to see if the message uses a mention prefix
+    const mentionPrefix =
+    (client.prefixMention.exec(message.content)) ?
+    message.content.match(client.prefixMention) : false;
+
+    // Use mention prefix otherwise use resolved prefix
+    prefix = (mentionPrefix) ? mentionPrefix[0] : prefix;
+
+    // If no prefix short circuit
+    return prefix || null;
+}
+
+// Put all the functions together to make an order
+async function order(client: Client, message: Message): Promise<Order | null> {
+
+    const [userConf, guildConf] = await getConf(client, message);
+
+    const prefix = getPrefix(client, message, userConf, guildConf);
+
+    if (!prefix) return null;
+
+    return {
+        message,
+        command,
+        permLevel,
+        prefix,
+        args,
+        rawArgs,
+        userConf,
+        guildConf,
+    };
+}
+
+// The the order stream
 export default (client: Client) =>
     fromEvent<Message>(client, 'message').pipe(
-
-        filter(msg => !msg.author.bot),
-
-        flatMap(message => async function order(): Promise<Order | null> {
-
-            // Get userConf or make userConf
-            const userConf: UserConfig = await client.userConf.findOneById(message.author.id)
-            || await client.userConf.save({ id: message.author.id });
-
-            // If dm set guildConf to 'DM'
-            const guildConf: GuildConfig | 'DM' =
-            (message.channel.type !== 'text') ? 'DM' :
-
-            // Get guildConf or make guildConf
-            await client.guildConf.findOneById(message.guild.id)
-            || await client.guildConf.save({ id: message.guild.id });
-
-            // Var to store the resolved prefix
-            let prefix = '';
-
-            // No prefix in dms
-            if (guildConf === 'DM')
-                prefix = '';
-
-            // Find prefix if not dms
-            else {
-
-                // Array to store found prefixes
-                const prefixes: string[] = [];
-
-                // If userConf has prefix push it
-                if (userConf.prefix)
-                    prefixes.push(userConf.prefix);
-
-                // If guildConf has prefix push it
-                if (guildConf.prefix)
-                    prefixes.push(guildConf.prefix);
-
-                // For each of the prefixes check if the message starts with it
-                for (const thisPrefix of prefixes)
-                    if (message.content.startsWith(thisPrefix))
-                        prefix = thisPrefix;
-
-                // Check to see if the message uses a mention prefix
-                const mentionPrefix =
-                (client.prefixMention.exec(message.content)) ?
-                message.content.match(client.prefixMention) : false;
-
-                // Use mention prefix otherwise use resolved prefix
-                prefix = (mentionPrefix) ? mentionPrefix[0] : prefix;
-
-                // If no prefix short circuit
-                if (!prefix)
-                    return null;
-
-            }
-
-            return {
-                message,
-                command,
-                permLevel,
-                prefix,
-                args,
-                rawArgs,
-                userConf,
-                guildConf,
-            };
-        }()),
-
-        filter(order => order != null),
+        filter(msg => !msg.author.bot), // Filter out bots
+        flatMap(msg => order(client, msg)), // Map to orders
+        filter(ord => ord != null), // Filter out null orders
     );
