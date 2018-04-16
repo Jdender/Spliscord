d91595982d5deb37bb713d4463c97d85c9613b5a
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-16 10:49:10 -0400

    Add info command

---
+++ src/imports/commands/info.ts
@@ -0,0 +1,26 @@
+import { Client } from 'discord.js';
+
+export default (client: Client) => {
+
+    client.registry.addCommand({
+        name: 'info',
+        description: 'Get various links for the bot.',
+        aliases: ['invite', 'link', 'links'],
+        usage: '',
+        cooldown: 3,
+        permissions: 0,
+        args: null,
+        checks: {
+            guildOnly: false,
+        },
+    });
+
+    client.registry.on('info', ({message}) =>
+    message.channel.send(
+`**Spliscord** - Made by jdender~/House Master (Dual mains)
+
+Invite Link: <${client.inviteLink}>
+GitHub: <${require('../../../package.json').homepage}>
+        `));
+
+};
