dbaacfaebe6f5ee55bfe344bb7d279d7a9496577
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-17 17:24:46 -0400

    Add String.clean

--- src/utill.ts
+++ src/utill.ts
@@ -1,11 +1,27 @@
-export default null;
+import { inspect } from 'util';
 
 declare global {
+
+    interface StringConstructor {
+        clean(text: any): Promise<string>;
+    }
 
     interface Array<T> {
         equals(array: any[]): boolean;
     }
 }
+
+String.clean = async (text: any): Promise<string> => {
+
+    if (text instanceof Promise) text = await text;
+
+    if (typeof text !== 'string') text = inspect(text);
+
+    return text
+    .replace(/`/g, '`' + String.fromCharCode(8203))
+    .replace(/@/g, '@' + String.fromCharCode(8203))
+    .replace(process.env.TOKEN, 'mfa.VkOb2v3T--NO--lWetW8tjND--TOKEN--QFTm--FOR--zq9PH--YOU--tG');
+};
 
 // From https://stackoverflow.com/a/14853974
 Array.prototype.equals = function(array: any[]) {
