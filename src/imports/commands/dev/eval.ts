71f254537bb76ca2fcbfd47b9177ba32df21104b
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-17 17:25:23 -0400

    Add dev.eval.js command

---
+++ src/imports/commands/dev/eval.ts
@@ -0,0 +1,34 @@
+import { Client } from 'discord.js';
+
+export default (client: Client) => {
+
+    client.registry.addCommand({
+        name: 'dev.eval.js',
+        description: 'Run some JavaScript in the bot\'s context.',
+        aliases: [],
+        usage: '[...code]',
+        cooldown: 0,
+        permissions: 5,
+        args: null,
+        checks: {
+            guildOnly: false,
+        },
+    });
+
+    client.registry.on('dev.eval.js', async ({message, prefix}) => {
+        try {
+            const args = message.channel.type === 'text'
+            ? message.content.slice(prefix.length).split(/\s+/g)
+            : message.content.split(/\s+/g);
+
+            args.splice(0, 3);
+            const code = args.join(' ');
+            const evaled = eval(code); // tslint:disable-line:no-eval
+
+            message.channel.send(await String.clean(evaled), { code: 'js' });
+        } catch (err) {
+            message.channel.send(`\`ERROR\` \`\`\`xl\n${await String.clean(err)}\n\`\`\``);
+        }
+    });
+
+};
