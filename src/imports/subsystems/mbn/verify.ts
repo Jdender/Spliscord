1bc9de757459af81131da2faeec541f3f823a0e4
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-17 08:15:17 -0400

    Some more fixes

--- src/imports/subsystems/mbn/verify.ts
+++ src/imports/subsystems/mbn/verify.ts
@@ -8,11 +8,10 @@ export default (client: Client) =>
         if (message.channel.id !== '427303825171415050') return;
         if (!message.content.match(/i confirm that i am over 18 years old\.?/i)) return;
 
-        if (!message.member.roles.has('425092490916397067'))
+        if (!message.member.roles.has('425092490916397067') && message.member.roles.has('415221406452809728'))
             return message.channel.send('You\'re already verified!');
 
-        message.member.removeRole(message.guild.roles.get('425092490916397067')!);
-        message.member.addRole(message.guild.roles.get('415221406452809728')!);
+        message.member.setRoles(['415221406452809728']);
 
         message.reply('Welcome, you\'re verified!');
     });
