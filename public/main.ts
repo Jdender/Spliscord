0d7b6ab47cd10b403ddbc4661cab28479fdb02e1
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-04-23 19:42:27 -0400

    Start topbar

--- public/main.ts
+++ public/main.ts
@@ -1,19 +1,7 @@
 import { Application, Controller } from 'stimulus';
 
-class Hello extends Controller {
-
-    public static targets = [ 'name' ];
-
-    private nameTarget: HTMLInputElement;
-
-    public greet() {
-      console.log(`Hello, ${this.name}!`); // tslint:disable-line:no-console
-    }
-
-    private get name() {
-      return this.nameTarget.value;
-    }
+class Topbar extends Controller {
 }
 
 const app = Application.start();
-app.register('hello', Hello);
+app.register('topbar', Topbar);
