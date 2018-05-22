9d120d256e1899fc0e69e36212105e341e02565b
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-05-22 17:35:05 -0400

    Finish auth's modRole and adminRole check

--- src/classes/auth.ts
+++ src/classes/auth.ts
@@ -1,10 +1,11 @@
 import { Client, Message } from 'discord.js';
+import { GuildConfig } from './settings';
 
 interface PermsObject {
     level: number;
     name: string;
     guildOnly: boolean;
-    check: (client: Client, message: Message) => boolean;
+    check: (client: Client, message: Message, guildConf: GuildConfig) => boolean;
 }
 
 export class Auth {
@@ -20,13 +21,31 @@ export class Auth {
             level: 1,
             name: 'Server Moderator',
             guildOnly: true,
-            check: (client, message) => false, // TODO
+            check: (client, message, guildConf) => {
+
+                if (!guildConf.modRole) return false;
+
+                const role = message.guild.roles.get(guildConf.modRole);
+
+                if (!role) return false;
+
+                return message.member.roles.has(role.id);
+            },
         },
         {
             level: 2,
             name: 'Server Administrator',
             guildOnly: true,
-            check: (client, message) => false, // TODO
+            check: (client, message, guildConf) => {
+
+                if (!guildConf.adminRole) return false;
+
+                const role = message.guild.roles.get(guildConf.adminRole);
+
+                if (!role) return false;
+
+                return message.member.roles.has(role.id);
+            },
         },
         {
             level: 3,
@@ -54,7 +73,7 @@ export class Auth {
         },
     ];
 
-    public checkPerms(client: Client, message: Message) {
+    public checkPerms(client: Client, message: Message, guildConf: GuildConfig | 'DM') {
         let permLevel = 0;
 
         const perms = [...this.perms];
@@ -66,7 +85,7 @@ export class Auth {
             if (!message.guild && currentLevel.guildOnly)
                 continue;
 
-            if (currentLevel.check(client, message)) {
+            if (currentLevel.check(client, message, guildConf)) {
                 permLevel = currentLevel.level;
                 break;
             }
