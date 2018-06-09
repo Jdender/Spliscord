0851c0c686a508efab6e2fb23a93b5b54d2c92bb
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-06-09 08:17:39 -0400

    Refactor conts out of fetch's class

--- src/imports/fetch.ts
+++ src/imports/fetch.ts
@@ -2,7 +2,18 @@ import { Client } from '../structures/Client';
 import { Command, KlasaMessage, Timestamp } from 'klasa';
 import { MessageEmbed, GuildMember, Guild } from 'discord.js';
 
+
+const statuses = {
+    online: '<:online:313956277808005120> Online',
+    idle: '<:away:313956277220802560> Idle',
+    dnd: '<:dnd:313956276893646850> Do Not Disturb',
+    offline: '<:offline:313956277237710868> Offline'
+};
+
+const timestamp = new Timestamp('d MMMM YYYY');
+
 type Target = GuildMember | Guild;
+
 
 export default (client: Client) => {
 
@@ -12,24 +23,15 @@ export default (client: Client) => {
         usage: '[targetMember:member|targetGuild:guild]'
     })
     class Fetch extends Command {
-
-        public statuses = {
-			online: '<:online:313956277808005120> Online',
-			idle: '<:away:313956277220802560> Idle',
-			dnd: '<:dnd:313956276893646850> Do Not Disturb',
-			offline: '<:offline:313956277237710868> Offline'
-        };
         
-        public timestamp = new Timestamp('d MMMM YYYY');
-        
-        async run(msg: KlasaMessage, [target]: [Target]) {
+        async run(message: KlasaMessage, [target]: [Target]) {
 
             let embed: MessageEmbed | null = null;
 
             if (target instanceof GuildMember) embed = this.member(target);
             if (target instanceof Guild) embed = this.guild(target);
     
-            return embed ? msg.sendEmbed(embed) : msg.send('Target not found');
+            return embed ? message.sendEmbed(embed) : message.send('Target not found');
         }
 
         member(target: GuildMember) {
@@ -38,9 +40,9 @@ export default (client: Client) => {
             .setThumbnail(target.user.displayAvatarURL())
             .addField('❯ Name', target.user.tag, true)
             .addField('❯ ID', target.id, true)
-            .addField('❯ Discord Join Date', this.timestamp.display(target.user.createdAt), true)
-            .addField('❯ Server Join Date', this.timestamp.display(target.joinedTimestamp), true)
-            .addField('❯ Status', this.statuses[target.presence.status], true)
+            .addField('❯ Discord Join Date', timestamp.display(target.user.createdAt), true)
+            .addField('❯ Server Join Date', timestamp.display(target.joinedTimestamp), true)
+            .addField('❯ Status', statuses[target.presence.status], true)
             .addField('❯ Playing', target.presence.activity ? target.presence.activity.name : 'N/A', true)
             .addField('❯ Highest Role', target.roles.size > 1 ? target.roles.highest.name : 'None', true)
             .addField('❯ Hoist Role', target.roles.hoist ? target.roles.hoist.name : 'None', true);
@@ -52,7 +54,7 @@ export default (client: Client) => {
             .setThumbnail(target.iconURL())
             .addField('❯ Name', target.name, true)
             .addField('❯ ID', target.id, true)
-            .addField('❯ Server Created Date', this.timestamp.display(target.createdTimestamp), true);
+            .addField('❯ Server Created Date', timestamp.display(target.createdTimestamp), true);
         }
     }
 };
