50cab002b3522fe5c82d958c77b60c1cf41f57bd
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-06-15 18:32:32 -0400

    Move client init to root.ts from Client.ts

--- src/root.ts
+++ src/root.ts
@@ -4,6 +4,12 @@ process
 .on('unhandledRejection', e => console.error(`UNHANDLED REJECTION: ${e.stack || e}`)) 
 .on('uncaughtException', e => console.error(`UNCAUGHT EXCEPTION: ${e.stack || e}`));
 
-import { client } from './structures/Client';
+import './util/patchStore';
+import { KlasaClient } from 'klasa';
 
-client.login(process.env.TOKEN!);
+// Change klasa client options here
+new KlasaClient({
+    prefix: '`',
+    readyMessage: (client: KlasaClient) => `${client.user.tag}, Runing in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`,
+})
+.login(process.env.TOKEN!);
--- src/structures/Client.ts
+++
@@ -1,7 +0,0 @@
-import { KlasaClient } from 'klasa';
-
-// Change klasa client options here
-export const client = new KlasaClient({
-    prefix: '`',
-    readyMessage: (client: KlasaClient) => `${client.user.tag}, Runing in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`,
-});
