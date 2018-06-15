89f7b715ea6164a763277df21ac2754d7e0a0eb7
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-06-15 19:29:10 -0400

    Fix updateActivity.ts

--- src/events/updateActivity.ts
+++ src/events/updateActivity.ts
@@ -2,9 +2,8 @@ import applyOptions from '../util/applyOptions';
 import { Event } from 'klasa';
 
 @applyOptions({
-    type: 'commands',
-    name: 'backdoor',
-    usage: '<Guild:guild>',
+    name: 'updateActivity',
+    event: 'guildMemberAdd',
 })
 export default class extends Event {
 
@@ -12,5 +11,10 @@ export default class extends Event {
         this.client.user.setActivity(
         `@Spliscord help | ${this.client.guilds.size} Guilds, ${this.client.channels.size} Channels, ${this.client.users.size} Users.`
         );
+    }
+
+    async init() {
+        // Update activity on ready
+        this.run();
     }
 }
