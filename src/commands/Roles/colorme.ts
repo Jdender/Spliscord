9688578d729346fae75e936c730839202755c5c2
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-07-01 22:10:22 -0400

    Comment and un spagety colorme

--- src/commands/Roles/colorme.ts
+++ src/commands/Roles/colorme.ts
@@ -1,12 +1,18 @@
 import { applyOptions } from '../../util/applyOptions';
-import { Command, KlasaMessage, CommandOptions } from 'klasa';
+import { Command, KlasaMessage } from 'klasa';
+import { GuildMember } from 'discord.js';
 
 @applyOptions({
     name: 'colorme',
     description: '',
     usage: '<Color:color>',
     requiredPermissions: ['MANAGE_ROLES'],
-} as CommandOptions)
+})
+
+// I use names to determine color roles, it's easier than storing a array in db
+// A color role is any role that starts with "§" and is followed by a hex color
+// e.g. "§#123123" is a valid color role name
+
 export default class extends Command {
 
     async run(message: KlasaMessage, [color]: [string]) {
@@ -15,54 +21,76 @@ export default class extends Command {
         if (!message.guildConfigs.get('allowColorme'))
             return message.send('The guild admins disabled or never enabled the `allowColorme` config option.');
 
-        const roles = await this.getRoles(message, color);
+        // Discord.js can't do more than one role action quickly
+        // So insted of adding/removing roles, we add/remove to an array
+        // Then set the members roles to said array, therefor only doing one role action
 
-        // Set the members roles to the array made earlyer
-        await message.member.roles.set(roles, 'Set color roles');
+        // Compute the new roles in an array
+        return this.getNewRoles(message, color)
 
-        await Promise.all(this.removeExtra(message));
+        // Set the member's roles to the array made earlyer
+        .then(roles => message.member.roles.set(roles, 'Set color roles'))
 
-        return message.send(`Swoosh! You're now painted \`${color}\`.`);
+        // The .then above this returns a member
+        // And removeExtra takes a member
+        // So we can just pass it to .then
+        .then(this.removeExtra)
+
+        // Last but not least
+        // Send a message confirming the color
+        .then(() => message.send(`Swoosh! You're now painted \`${color}\`.`))
+        
+        // If any of the above steps threw a error,
+        // Send a error message
+        .catch(() => message.send('Sorry, I was unable to paint you.'));
     }
 
     // Get the roles of the member with the color role included
-    async getRoles(message: KlasaMessage, color: string) {
+    async getNewRoles(message: KlasaMessage, color: string) {
 
         // See if there is a color role with the same color
-        const existing = message.guild.roles.find(role => role.name === `§${color}`);
-        const roles = this.getInitRoles(message);
+        // If existing make an array with it and all the non color roles
+        // If not create one and do the same
+        return [
+            ...this.getMemberNonColorRoles(message),
+            // Find *or* create one
+            this.findExistingColorRole(message, color) || await this.createColorRole(message, color),
+        ];
+    }
 
-        // If existing push it to roles and return
-        if (existing) return [...roles, existing ];
+    // Try to find a color role with the same color
+    findExistingColorRole(message: KlasaMessage, color: string) {
 
-        // If not create one and push-return
-        return [...roles, await this.createRole(message, color) ];
+        return message.guild.roles
+        .find(role => role.name === `§${color}`); // Find using color role name format
     }
 
     // The roles the member allredy has, filtering out any existing color roles
-    getInitRoles(message: KlasaMessage) {
+    getMemberNonColorRoles(message: KlasaMessage) {
 
         return message.member.roles
-        .array().filter(role => !role.name.startsWith('§#'));
+        .filter(role => !role.name.startsWith('§#')) // Use name to determine color role or not
+        .array(); // We want to return a array not a collection
     }
 
     // Creates a role with color and name
-    createRole(message: KlasaMessage, color: string) {
+    createColorRole(message: KlasaMessage, color: string) {
 
         return message.guild.roles.create({
             data: {
-                name: `§${color}`,
+                name: `§${color}`, // Use color role name format
                 color,
                 mentionable: false,
-                hoist: false,
+                hoist: false, // "Display role members separately from online members"
             },
             reason: 'Create color role',
         });
     }
 
     // Remove any unused color roles in guild
-    removeExtra(message: KlasaMessage) {
-        return message.guild.roles // Guild roles
+    removeExtra(member: GuildMember) {
+
+        return member.guild.roles // Guild roles
         .filter(role => role.name.startsWith('§#')) // Filter for color roles
         .filter(role => role.members.size === 0) // Filter for empty roles
         .map(role => role.delete('Unused color role')); // Delete roles
