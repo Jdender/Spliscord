9a30c8b90573656448ac3a48e9e46dfc2462b373
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-07-05 19:58:40 -0400

    Add remind command

---
+++ src/commands/Tools/remind.ts
@@ -0,0 +1,26 @@
+import { applyOptions } from '../../util/applyOptions';
+import { Command, KlasaMessage } from 'klasa';
+
+@applyOptions({
+    name: 'remind',
+    description: 'Get the fisrt url that shows up from searching a query.',
+    usage: '<When:time> <Reminder:string> [...]',
+    usageDelim: ' ',
+    aliases: ['reminder', 'remindme'],
+})
+export default class extends Command {
+
+    async run(message: KlasaMessage, [when, ...text]: string[]) {
+
+        this.client.schedule.execute
+
+        const reminder = await this.client.schedule.create('reminder', when, {
+			data: {
+                message,
+                text,
+			},
+        });
+        
+		return message.send(`Okay, I created you a reminder with the id: \`${reminder.id}\``);
+    }
+}
