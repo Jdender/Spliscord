5ec1cac8045ee02d7bb6b405b290971efa08c36f
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-08-11 12:03:36 -0400

    Format patchStore

--- src/util/patchStore.ts
+++ src/util/patchStore.ts
@@ -1,28 +1,35 @@
 import { Store } from 'klasa';
 import fs = require('fs-nextra');
-import { extname, relative, sep } from 'path';
+import { extname, relative, sep, join } from 'path';
 
 // Files that can be required
-const extensions = Object.keys(require.extensions).filter(key => key !== '.json' && key !== '.node');
+const extensions = Object
+.keys(require.extensions)
+.filter(key => !['.json', '.node'].includes(key));
             
 // Make sure file is a file and can be required
-const filter = (stats: any, path: string) => stats.isFile() && extensions.includes(extname(path));
+const filter = (stats: any, path: string) => 
+    stats.isFile() && 
+    extensions.includes(extname(path));
 
 // Monkeypatch klasa Store
 (Store as any).walk = async (store: any, core: boolean) => {
 
     const dir = core ? store.coreDir : store.userDir;
-            
+
     const files = await fs.scan(dir, { filter })
     .catch(() => {}); // Ignore errors
-             
+
     if (!files) return true;
 
-    const load = [...files.keys()]
-    .map(file => store.load(
-            relative(dir, file).split(sep), core,
+    const load = Array
+    .from(files.keys())
+    .map(file => 
+        store.load(
+            relative(dir, file).split(sep), 
+            core,
         ),
     );
-     
+
     return Promise.all(load);
 };
