cd7e37b8918d7619c7e81d18b49ee1f7710543a2
James <jdenderplays@gmail.com>
2018-08-27 21:45:16 -0400

    Upgrade colorme to have a configuable name

--- src/commands/Roles/colorme.ts
+++ src/commands/Roles/colorme.ts
@@ -10,12 +10,11 @@ import { GuildMember } from 'discord.js';
 })
 
 // I use names to determine color roles, it's easier than storing a array in db
-// A color role is any role that starts with "§" and is followed by a hex color
-// e.g. "§#123123" is a valid color role name
+// A color role is any role that matches the guild's colormeRoleName
 
 export default class extends Command {
 
-    async run(message: KlasaMessage, [color]: [string]) {
+    async run(message: KlasaMessage, [colorStr]: [string]) {
         
         if (!message.guild.settings.get('allowColorme'))
             return message.send('The guild admins disabled or never enabled the `allowColorme` config option.');
@@ -24,62 +23,69 @@ export default class extends Command {
         // So insted of adding/removing roles, we add/remove to an array
         // Then set the members roles to said array, therefor only doing one role action
 
+        const name: string = message.guild.settings.get('colormeRoleName');
+
+        const color = parseInt(colorStr, 16);
+
         // Compute the new roles in an array
-        return this.getNewRoles(message, color)
+        return this.getNewRoles(message, color, name)
 
         .then(roles => message.member.roles.set(roles, 'Set color roles'))
 
-        .then(this.removeExtra(color))
+        .then(this.removeExtra(color, name))
 
-        .then(() => message.send(`Swoosh! You're now painted \`${color}\`.`))
+        .then(() => message.send(`Swoosh! You're now painted \`#${colorStr}\`.`))
         
         .catch(() => message.send('Sorry, I was unable to paint you.'));
     }
 
     // Get the roles of the member with the color role included
-    private getNewRoles = async (message: KlasaMessage, color: string) =>
+    private getNewRoles = async (message: KlasaMessage, color: number, name: string) =>
 
         [
             ...this.getMemberNonColorRoles(message),
-            this.findExistingColorRole(message, color) || await this.createColorRole(message, color),
+            this.findExistingColorRole(message, color, name) || await this.createColorRole(message, color, name),
         ];
     
 
     // Try to find a color role with the same color
-    private findExistingColorRole = (message: KlasaMessage, color: string) => 
+    private findExistingColorRole = (message: KlasaMessage, color: number, name: string) => 
 
         message.guild.roles
-        .find(role => role.name === `§${color}`);
+        .find(role => role.name === name && role.color === color);
     
 
     // The roles the member allredy has, filtering out any existing color roles
     private getMemberNonColorRoles = (message: KlasaMessage) => 
 
         message.member.roles
-        .filter(role => !role.name.startsWith('§#'))
+        .filter(role => role.name !== message.guild.settings.get('colormeRoleName'))
         .array();
     
-
+    
     // Creates a role with color and name
-    private createColorRole = (message: KlasaMessage, color: string) =>
+    private createColorRole = (message: KlasaMessage, color: number, name: string) =>
 
         message.guild.roles.create({
             data: {
-                name: `§${color}`,
+                name,
                 color,
                 mentionable: false,
                 hoist: false,
+                position: message.guild.settings.get('putColorRolesAtTop')
+                ? message.guild.me.roles.highest.position
+                : undefined,
             },
             reason: 'Create color role',
         });
 
 
     // Remove any unused color roles in guild
-    private removeExtra = (color: string) => (member: GuildMember) =>
+    private removeExtra = (color: number, name: string) => (member: GuildMember) =>
 
         member.guild.roles // Guild roles
-        .filter(role => role.name !== `§${color}`) // Filter out just created
-        .filter(role => role.name.startsWith('§#')) // Filter for color roles
+        .filter(role => role.name === name) // Filter for color roles
+        .filter(role => role.color !== color) // Filter out just created
         .filter(role => role.members.size === 0) // Filter for empty roles
         .map(role => role.delete('Unused color role')); // Delete roles
     
@@ -92,6 +98,15 @@ export default class extends Command {
 
         if (!schema.has('allowColorme'))
             await schema.add('allowColorme', { type: 'boolean' });
+
+        if (!schema.has('putColorRolesAtTop'))
+            await schema.add('putColorRolesAtTop', { type: 'boolean' });
+
+        if (!schema.has('colormeRoleName'))
+            await schema.add('colormeRoleName', { 
+                type: 'string',
+                default: 'Color',
+            });
     }
 
 }
